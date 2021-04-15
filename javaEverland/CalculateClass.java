5eb3f8cc3b55a483304f8fda0c78fb8774f89504
jiro-Park <6358727@hanmail.net>
2021-04-15 13:32:10 +0900

    amusementpark ticket selling cycle

---
+++ javaEverland/CalculateClass.java
@@ -0,0 +1,15 @@
+package javaEverland;
+
+public class CalculateClass {
+	private final int[]DAY_TICKET_PRICE = {56000,47000,44000,44000}; 
+	private final int[]NIGHT_TICKET_PRICE = {46000,40000,37000,37000};
+	private final int AGELIMIT_BABY = 2;
+	private final int AGELIMIT_CHILD = 12;
+	private final int AGELIMIT_ADOLESCENT = 18;
+	private final int AGELIMIT_ADULT = 64;
+	private final double DISABLED_DISCOUNT_RATE = 0.6;
+	private final double MERIT_DISCOUNT_RATE = 0.5;
+	private final double MULTICHILD_DISCOUNT_RATE = 0.8;
+	private final double  PREGNANT_DISCOUNT_RATE= 0.85;
+
+}
---
+++ javaEverland/CustomerInfo.java
@@ -0,0 +1,44 @@
+package javaEverland;
+
+public class CustomerInfo {
+	
+	private int ticketType;
+	private String residentNum;
+	private int ticketNum;
+	private int advantageType;
+	private int sum;
+	
+	
+	public final int getTicketType() {
+		return ticketType;
+	}
+	public final void setTicketType(int ticketType) {
+		this.ticketType = ticketType;
+	}
+	public final String getResidentNum() {
+		return residentNum;
+	}
+	public final void setResidentNum(String residentNum) {
+		this.residentNum = residentNum;
+	}
+	public final int getTicketNum() {
+		return ticketNum;
+	}
+	public final void setTicketNum(int ticketNum) {
+		this.ticketNum = ticketNum;
+	}
+	public final int getAdvantageType() {
+		return advantageType;
+	}
+	public final void setAdvantageType(int advantageType) {
+		this.advantageType = advantageType;
+	}
+	public final int getSum() {
+		return sum;
+	}
+	public final void setSum(int sum) {
+		this.sum = sum;
+	}
+
+
+}
